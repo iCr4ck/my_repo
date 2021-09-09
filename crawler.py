e32909159a8d1b17e85184218326b9269a994ba5
Dominik Scheuer <59667939+iCr4ck@users.noreply.github.com>
2021-09-09 18:33:47 +0200

    Add files via upload

---
+++ crawler.py
@@ -0,0 +1,18 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+r = requests.get("https://computer-nach-mass.de/html/firmenprofil.html")
+
+html = """<html> <p>Ich bin ein Absatz</p>
+
+            </html>
+            """
+
+
+doc = BeautifulSoup(html,"html.parser")
+
+print(doc.find_all("p"))
+
+for x  in doc.find_all("p"):
+    print(x.attrs)
